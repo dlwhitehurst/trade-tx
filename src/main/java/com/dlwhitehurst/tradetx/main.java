ed430585d9f6c8956bf8a8c82d4e2f8b802bcfb6
David L Whitehurst <me@dlwhitehurst.com>
2023-04-23 14:40:31 -0400

    swagger docs rendering

--- src/main/java/com/dlwhitehurst/tradetx/main.java
+++ src/main/java/com/dlwhitehurst/tradetx/main.java
@@ -10,8 +10,10 @@ package com.dlwhitehurst.tradetx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class main {
 
 	public static void main(String[] args) {
